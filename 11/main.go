7080be6c4c522955579c1cdf893d68358e0a6c30
guilherme <vinespedro55@gmail.com>
2023-12-18 22:31:09 -0300

    feat: add func in struct

--- 11/main.go
+++ 11/main.go
@@ -10,12 +10,18 @@ type Cliente struct{
 	Ativo bool
 }
 
+func (c Cliente) Desativar(){
+	c.Ativo =false
+	fmt.Print("cliente desativado")
+}
+
 func main(){
 	gui := Cliente{
 		Nome: "Guilhermme",
 		Idade: 22,
-		Ativo: false,
+		Ativo: true,
 	}
+	gui.Desativar()
 	fmt.Printf("Nome: %s, Idade %d, ativo: %t",gui.Nome,gui.Idade,gui.Ativo)
 }
 
